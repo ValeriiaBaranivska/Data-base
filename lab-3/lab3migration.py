0c470151a4599c23376f0bc8517dd7276881bb6b
Valeria Baranivska <v.baranivska.km23@kpi.ua>
2025-03-11 19:36:23 +0200

    step-2-refactor-base

--- lab-3/lab3migration.py
+++ lab-3/lab3migration.py
@@ -1,33 +1,24 @@
 import pandas as pd
-from sqlalchemy import create_engine, Column, Integer, Float, String, Enum, Date, Time
+from sqlalchemy import ForeignKey, create_engine, Column, Integer, Float, String, Enum, Date, Time
 from sqlalchemy.orm import sessionmaker, declarative_base
 import enum
 import psycopg2
-
-pd.set_option('display.max_columns', None)     # показувати всі колонки
-pd.set_option('display.max_rows', None)        # показувати всі рядки (тут на всяк випадок)
-pd.set_option('display.max_colwidth', None)    # не обрізати значення в колонках
-pd.set_option('display.expand_frame_repr', False)  # не переносити фрейм на новий рядок
+import alembic
 
 # Крок 1: Читаємо CSV файл
 df = pd.read_csv("GlobalWeatherRepository.csv")
 
 # виведення списку хедерів, щоб визначити відповідно до категорії
 list_of_column_names = list(df.columns)
-print('Список хедерів : ',
-      list_of_column_names)
-print(df['wind_direction'].unique())
+#print('Список хедерів : ',
+#      list_of_column_names)
+#print(df['wind_direction'].unique())
 
 # Перевіряємо наявність необхідних колонок у CSV файлі
 required_columns = ['country', 'wind_degree', 'wind_kph',
                     'wind_direction', 'last_updated', 'sunrise',
                     'precip_mm', 'precip_in']
 #print(df[required_columns].head(10))
-
-for col in required_columns:
-    if col not in df.columns:
-        print(f"Відсутня колонка: {col}")
-        exit(1)
 
 # Дані для підключення та створення БД
 DB_NAME = "globalweather"
@@ -36,7 +27,7 @@ DB_PASSWORD = "adopted29v"
 DB_HOST = "localhost"
 DB_PORT = "5432"
 
-# Ствоерення БД
+# Створення БД
 conn = psycopg2.connect(
    database="postgres", user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT
 )
@@ -75,23 +66,35 @@ class WindDirectionEnum(enum.Enum):
     WNW = 'WNW'
 
 # опис ORM
+class Country(Base):
+    __tablename__ = 'country'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+
+class PrecipNum(Base):
+    __tablename__ = 'precipitate'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    precip_mm = Column(Float, nullable=True)
+    precip_in = Column(Float, nullable=True)
+
 class WeatherData(Base):
     __tablename__ = 'weather_data'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    country = Column(String, nullable=False)
     wind_degree = Column(Integer, nullable=False)
     wind_kph = Column(Float, nullable=False)
     wind_direction = Column(Enum(WindDirectionEnum), nullable=False)
     last_updated = Column(Date, nullable=False)
     sunrise = Column(Time, nullable=False)
-    precip_mm = Column(Float, nullable=True)
-    precip_in = Column(Float, nullable=True)
 
+    country_id = Column(Integer, ForeignKey('country.id'), nullable=False)
+    precip_mm_id = Column(Integer, ForeignKey('precipitate.id'))
+    precip_in_id = Column(Integer, ForeignKey('precipitate.id'))
 # Створюємо SQLAlchemy engine до нової бази
 engine = create_engine(f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
 
 # Створення таблиці
+#WeatherData.__table__.drop(engine)
 Base.metadata.create_all(engine)
 
 # Ініціалізуємо сесію
@@ -100,17 +103,35 @@ session = Session()
 
 # Перетворюємо дані з DataFrame в об'єкти ORM та додаємо їх до бази даних
 for index, row in df.iterrows():
-    new_record = WeatherData(
-        country=row['country'],
+    country = session.query(Country).filter_by(name=row['country']).first()
+    if not country:
+        country = Country(name=row['country'])
+        session.add(country)
+        session.commit()
+
+    precip_mm_num = session.query(PrecipNum).filter_by(precip_mm=row['precip_mm']).first()
+    precip_in_num = session.query(PrecipNum).filter_by(precip_in=row['precip_in']).first()
+    if not precip_mm_num:
+        precip_mm_num = PrecipNum(precip_mm=row['precip_mm'])
+        session.add(precip_mm_num)
+
+    if not precip_in_num:
+        precip_in_num = PrecipNum(precip_in=row['precip_in'])
+        session.add(precip_in_num)
+
+    # Додавання погодних даних
+    weather_data = WeatherData(
         wind_degree=row['wind_degree'],
         wind_kph=row['wind_kph'],
-        wind_direction=WindDirectionEnum(row['wind_direction']),
+        wind_direction=row['wind_direction'],
         last_updated=pd.to_datetime(row['last_updated']).date(),
         sunrise=pd.to_datetime(row['sunrise']).time(),
-        precip_mm=row['precip_mm'],
-        precip_in=row['precip_in']
+
+        country_id=country.id,
+        precip_mm_id = precip_mm_num.id,
+        precip_in_id = precip_in_num.id
     )
-    session.add(new_record)
+    session.add(weather_data)
 
 # Зберігаємо зміни в базі даних
 session.commit()
