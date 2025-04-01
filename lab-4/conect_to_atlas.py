86b5b1074dbb3d2fb42a0d486ec5cff91e45f5f2
Valeria Baranivska <v.baranivska.km23@kpi.ua>
2025-04-01 15:26:04 +0300

    add order to atlas

--- lab-4/conect_to_atlas.py
+++ lab-4/conect_to_atlas.py
@@ -1,5 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from insertion import items, orders
+
 
 uri = "mongodb+srv://baranivskavaleriia:RMIPnOaGRnWJjnu9@cluster0.w5gsc40.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
@@ -19,4 +21,13 @@ db = client["test-1"]
 # колекція товарів
 collection_item = db["col-1"]
 
+collection_item.insert_many(items)
+print("Товари успішно додані!")
 
+# колекція замовлень
+coll_order = db["col-2"]
+
+coll_order.insert_many(orders)
+print("Замовлення успішно додані!")
+
+#print(db.list_collection_names())
--- lab-4/insertion.py
+++ lab-4/insertion.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from bson import ObjectId
+
 items = [
         {
             "category": "Phone",
@@ -43,4 +46,72 @@ items = [
         }
     ]
 
-orders = []
+orders = [
+    {
+    "order_number" : 1,
+    "date" : datetime(2015,4,14),
+    "total_sum" : 1923.4,
+    "customer" : {
+        "name" : "Andrii",
+        "surname" : "Rodinov",
+        "phones" : [ 9876543, 1234567],
+        "address" : "PTI, Peremohy 37, Kyiv, UA"
+    },
+    "payment" : {
+        "card_owner" : "Andrii Rodionov",
+        "cardId" : 12345678
+    },
+    "items_id" : [ObjectId("67eba78754efd3600024bf59"), ObjectId("67eba78754efd3600024bf5e")]
+    },
+
+    {
+    "order_number" : 2,
+    "date" : datetime(2016,2,12),
+    "total_sum" : 60000,
+    "customer" : {
+        "name" : "Valeriia",
+        "surname" : "Baranivska",
+        "phones" : [ 9876543, 1234567],
+        "address" : "Novo Polova 321, Lviv, UA"
+    },
+    "payment" : {
+        "card_owner" : "Valeriia Baranivska",
+        "cardId" : 12344378278
+    },
+    "items_id" : [ObjectId("67eba78754efd3600024bf5e"), ObjectId("67eba78754efd3600024bf5b")]
+    },
+
+    {
+    "order_number" : 3,
+    "date" : datetime(2018,3,9),
+    "total_sum" : 23291,
+    "customer" : {
+        "name" : "Charli",
+        "surname" : "Terak",
+        "phones" : [ 7329482, 362749],
+        "address" : " Rethord 37 ave, London, UK"
+    },
+    "payment" : {
+        "card_owner" : "Charli West",
+        "cardId" : 73284926
+    },
+    "items_id" : [ObjectId("67eba78754efd3600024bf5d"), ObjectId("67eba78754efd3600024bf5a")]
+    },
+
+    {
+    "order_number" : 4,
+    "date" : datetime(2015,4,14),
+    "total_sum" : 500,
+    "customer" : {
+        "name" : "Peter",
+        "surname" : "Smith",
+        "phones" : [ 3047293810, 203886482],
+        "address" : "Leet st 21/23, Poland"
+    },
+    "payment" : {
+        "card_owner" : "Peter Smith",
+        "cardId" : 90381941
+    },
+    "items_id" : [ObjectId("67eba78754efd3600024bf5b"), ObjectId("67eba78754efd3600024bf5b")]
+    }
+]
