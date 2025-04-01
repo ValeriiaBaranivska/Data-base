38a375d892cc6be62790924389a296072922d247
Valeria Baranivska <v.baranivska.km23@kpi.ua>
2025-04-01 17:29:17 +0300

    fix ipynd file

--- lab-4/capped_collection.py
+++ lab-4/capped_collection.py
@@ -1,8 +1,15 @@
 from conect_to_atlas import db
 from datetime import datetime
+from pymongo.errors import CollectionInvalid
 
 cc_3 = "reviews_capped"
-db.create_collection(cc_3, capped=True, size=100000, max=5)
+
+try:
+    db.create_collection(cc_3, capped=True, size=100000, max=5)
+    print(f"Capped collection '{cc_3}' створено.")
+except CollectionInvalid:
+    print(f"Capped collection '{cc_3}' вже існує.")
+
 
 print(f"Capped collection '{cc_3}' cтворено")
 
@@ -17,13 +24,5 @@ def insert_review(collection, review_id, customer_name, review_text, rating):
     collection.insert_one(review)
     print(f"Відгук {review_id} додано")
 
-capped_collection = db[cc_3] # колекція відгуків
+capped_coll = db[cc_3] # колекція відгуків
 
-#Вставка більше 5 відгуків, для перевірки
-for i in range(1, 8):
-    insert_review(capped_collection, i, f"Користувач {i}", f"Відгук користувача на товар {i}", i * 1.0)
-
-reviews = capped_collection.find()
-print("Reviews in the capped collection:")
-for review in reviews:
-    print(review)
