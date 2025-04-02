afdc32ea5863358be05a7b87a1c6c8698c29ca6d
Valeria Baranivska <v.baranivska.km23@kpi.ua>
2025-04-02 20:57:33 +0300

    last update

--- lab-5/conect_to_neo4j.py
+++ lab-5/conect_to_neo4j.py
@@ -8,27 +8,28 @@ AUTH = ("neo4j", "cards-argument-purchases")
 
 driver = GraphDatabase.driver(URI, auth=basic_auth(*AUTH))
 
-
+# Функція для виконання довільного Cypher-запиту
 def execute_query(cypher_query, parameters=None):
-    """Функція для виконання довільного Cypher-запиту"""
     with driver.session(database="neo4j") as session:
         return session.read_transaction(
             lambda tx: tx.run(cypher_query, **(parameters or {})).data()
         )
+
+
+# Основна функція для створення вузлів і зв’язків у базі Neo4j
 def create_schema():
-    """Основна функція для створення вузлів і зв’язків у базі Neo4j"""
     with driver.session(database="neo4j") as session:
         create_items(session)
         create_customers(session)
         create_orders(session)
         create_relationships(session)
 
-
+# Додає товари / клієнтів / замовлення, якщо їх ще немає
 def create_items(session):
-    """Додає товари, якщо їх ще немає"""
     items = [
         {"id": 1, "name": "Laptop", "price": 1000},
-        {"id": 2, "name": "Smartphone", "price": 500}
+        {"id": 2, "name": "Smartphone", "price": 500},
+        {"id": 3, "name": "TV", "price": 2300}
     ]
     for item in items:
         session.execute_write(
@@ -39,12 +40,11 @@ def create_items(session):
         )
         print(f"Item {item['name']} added.")
 
-
 def create_customers(session):
-    """Додає клієнтів, якщо їх ще немає"""
     customers = [
         {"id": 1, "name": "Valeriia"},
-        {"id": 2, "name": "Tom"}
+        {"id": 2, "name": "Patrick"},
+        {"id": 3, "name": "Ivan"}
     ]
     for customer in customers:
         session.execute_write(
@@ -57,10 +57,12 @@ def create_customers(session):
 
 
 def create_orders(session):
-    """Додає замовлення, якщо їх ще немає"""
     orders = [
-        {"id": 1, "date": "2023-10-01"},
-        {"id": 2, "date": "2023-10-02"}
+        {"id": 1, "date": "2023-11-01"},
+        {"id": 2, "date": "2023-10-02"},
+        {"id": 3, "date": "2023-10-02"},
+        {"id": 4, "date": "2025-12-31"}
+
     ]
     for order in orders:
         session.execute_write(
@@ -71,12 +73,14 @@ def create_orders(session):
         )
         print(f"Order {order['id']} added.")
 
-
+# Додаємо звязки між товарами / клієнтами / замовленнями
 def create_relationships(session):
-    """Додає зв’язки між замовленнями, товарами та клієнтами"""
     relationships = [
         {"customer_id": 1, "order_id": 1},
-        {"customer_id": 2, "order_id": 2}
+        {"customer_id": 2, "order_id": 2},
+        {"customer_id": 2, "order_id": 3},
+        {"customer_id": 3, "order_id": 4},
+        {"customer_id": 4, "order_id": 3}
     ]
     for rel in relationships:
         session.execute_write(
@@ -90,7 +94,11 @@ def create_relationships(session):
     # Додаємо товари в замовлення
     order_items = [
         {"order_id": 1, "item_id": 1},  # Laptop -> Order 1
-        {"order_id": 2, "item_id": 2}   # Smartphone -> Order 2
+        {"order_id": 2, "item_id": 2},  # Smartphone -> Order 2
+        {"order_id": 3, "item_id": 3},  # TV -> Order 3
+        {"order_id": 4, "item_id": 2},  # Smartphone -> Order 4
+        {"order_id": 1, "item_id": 3},  # TV -> Order 1
+        {"order_id": 2, "item_id": 1}   # Laptop -> Order 2
     ]
     for rel in order_items:
         session.execute_write(
@@ -103,8 +111,10 @@ def create_relationships(session):
 
     # Додаємо перегляди товарів клієнтами
     views = [
-        {"customer_id": 1, "item_id": 2},  # Valeriia дивилася на смартфон
-        {"customer_id": 2, "item_id": 1}   # Tom дивився на ноутбук
+        {"customer_id": 1, "item_id": 2},  #  на смартфон
+        {"customer_id": 2, "item_id": 1},   #  на ноутбук
+        {"customer_id": 3, "item_id": 3},  # на телевізор
+        {"customer_id": 2, "item_id": 2}  # на смартфон
     ]
     for rel in views:
         session.execute_write(
